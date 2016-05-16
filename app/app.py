c353e092ec4bb714d8298cc90912b7d95ef59e33
Kyle <kyle.stratis@gmail.com>
2016-05-16 12:43:44 -0400

    Video 4 - request handling

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import requests
 
 # Flask instance
 app = Flask(__name__)
@@ -9,17 +10,32 @@ def hello():
     return "Hello, World!"
 
 # Must specifically add POST
+# @app.route("/", methods=['GET', 'POST'])
+# def home():
+#     # Checking flask request object for request type
+#     if request.method == 'POST':
+#         value_one = int(request.form.get('first'))
+#         value_two = int(request.form.get('second'))
+#         total = value_one + value_two
+#         data = {"total": str(total)}
+#         return jsonify(data)
+#     # GET request
+#     return render_template('index.html', string="TESTING!")
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    # Checking flast request object for request type
+    # Checking flask request object for request type
     if request.method == 'POST':
-        value_one = int(request.form.get('first'))
-        value_two = int(request.form.get('second'))
-        total = value_one + value_two
-        data = {"total": str(total)}
-        return jsonify(data)
+        # user input
+        value_one = request.form.get('first')
+        value_two = request.form.get('second')
+        # api call
+        url = 'https://api.github.com/search/users?q=location:{0}+language:{1}'.format(value_one, value_two)
+        response_dict = requests.get(url).json()
+        return jsonify(response_dict)
     # GET request
     return render_template('index.html', string="TESTING!")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/static/main.js
+++ app/static/main.js
@@ -3,9 +3,9 @@ $(document).ready(function() { // When DOM is fully loaded, convention
 
     $("form").on("submit", function() {
         console.log("form has been submitted!");
-        var valueOne = $('input[name="number-one"]').val();
-        var valueTwo = $('input[name="number-two"]').val();
-        console.log(valueOne,valueTwo);
+        var valueOne = $('input[name="location"]').val();
+        var valueTwo = $('input[name="language"]').val();
+        // console.log(valueOne,valueTwo);
 
         //AJAX request to send input data to backend
         $.ajax({
@@ -13,8 +13,8 @@ $(document).ready(function() { // When DOM is fully loaded, convention
             url: "/",
             data: {first: valueOne, second: valueTwo},
             success: function(results) {
-                console.log(results);
-                $("#results").html(results.total);
+                // console.log(results.items[0]);
+                $("#results").html('<a href="'+results.items[0].html_url+'">'+results.items[0].login+'</a><br><img src="'+results.items[0].avatar_url+'">');
                 // clear form input
                 $("input").val("");
             },
